a86c896c594fb58f8acffad2ffd6eae5172a9de4
bgoonz <bryan.guner@gmail.com>
2022-01-21 16:50:58 +0000

    update

---
+++ photogur/app/controllers/pictures_controller.rb
@@ -0,0 +1,29 @@
+class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # If the save worked, go to index.html.erb
+      redirect_to pictures_url
+    else
+      render :new
+    end
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+
+end
---
+++ photogur/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogur::Application.config.secret_key_base = '4c41f18869a750f1530787dfb486a540a5edcb30fe2297dd71d2a35891132d841fd6d79ee85abe96e72bee418bb0422b5a03096294ad2f125fd1f3f192ee9efe'
