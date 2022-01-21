62d84fd6ebf98a00a20fd7b03f3ff71ea226f287
bgoonz <bryan.guner@gmail.com>
2022-01-21 16:50:19 +0000

    update

---
+++ m2-1-js--fundamentals-P1/workshop/5-control-flow/5-exercise-4.4.js
@@ -0,0 +1,8 @@
+// -------------------------------------------------------------
+// Control Flow Exercises
+// ----------
+// note: need to have covered control flow for these exercises
+// -------------------------------------------------------------
+
+// Q4.4
+// Write a loop that will print to the console all of the EVEN integers from 100 to 0.
---
+++ m2-1-js--fundamentals-P1/workshop/5-control-flow/5-exercise-7.js
@@ -0,0 +1,32 @@
+// -------------------------------------------------------------
+// Control Flow Exercises
+// ----------
+// note: need to have covered control flow for these exercises
+// -------------------------------------------------------------
+
+// Q7
+// Write a program that goes through every number between 1 and 100, and follows the following rules:
+//   - If the number is divisible by 3 (eg. 6), print "Fizz"
+//   - If the number is divisible by 5 (eg. 10), print "Buzz"
+//   - If the number is divisible by 3 AND 5 (eg. 15), print "FizzBuzz"
+//   - For all other numbers, print the number itself.
+
+// e.g.
+//
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+// 16
+// ...
